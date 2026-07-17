8afd7dbaec8ab4aacdc2a273a71c878296e9604a
olshanskyev <olshanskyev@gmail.com>
2026-07-17 16:28:59 +0300

    Update groups meta

--- infra/db/init/04_groups_schema.sql
+++ infra/db/init/04_groups_schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE __DATABASE_SCHEMA__.groups (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name        VARCHAR(255) NOT NULL,
     owner_id    UUID NOT NULL,
+    avatar_url  TEXT,
     epoch       INTEGER NOT NULL DEFAULT 0,
     created_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
